64bfa3419dbc6150f1bdb6557b9bdb2dd22c9da1
Staff <staff@Radostinas-MacBook-Pro.local>
2017-01-11 15:38:55 -0500

    added media queries for floating bloc

---
+++ jquery/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.tab1').click(function(){
+    $('.divider').css("background", "rgba(17, 63, 103, .8)");
+  });
+});
+
+$(document).ready(function(){
+  $('.tab2').click(function(){
+    $('.divider').css("background", "rgba(0, 125, 194, .8)");
+  });
+});
+
+$(document).ready(function(){
+  $('.tab3').click(function(){
+    $('.divider').css("background", "rgba(19, 181, 234, .8)");
+  });
+});
